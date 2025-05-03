ede7cf07819b96ed572dd48a838e3e5d5c681adf
Matt Rich <m.rich137@gmail.com>
2025-05-03 13:26:23 -0400

    lesson 4 complete

--- graphics.py
+++ graphics.py
@@ -1,6 +1,34 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+        
+    def __str__(self):
+        return f"Point({self.x}, {self.y})"
+
+
+
+
+class Line:
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+        
+        
+    def __str__(self):
+        return f"Line({self.p1}, {self.p2})"
+    
+    
+    def draw(self, canvas: Canvas, fill_color="black"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+
+
+
+
 class Window:
     
     def __init__(self, width=800, height=600):
@@ -9,6 +37,10 @@ class Window:
         self.canvas = Canvas(self.__root, width=width, height=height)
         self.canvas.pack()
         self.running = False
+        
+        
+    def draw_line(self, line: Line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
 
     
     def redraw(self):
